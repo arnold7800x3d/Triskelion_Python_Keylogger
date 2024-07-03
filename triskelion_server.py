14c9f8c5ae5ac930fe4f0de5b7fbd748d2691d9a
Arnold Ochieng <arnold.7800x3d@gmail.com>
2024-07-03 19:11:57 +0300

    Update triskelion_server.py

--- triskelion_server.py
+++ triskelion_server.py
@@ -1,15 +1,15 @@
 from flask import Flask, request, redirect, url_for
 import os
 
-app = Flask(__name__)
+r_server = Flask(__name__)
 UPLOAD_FOLDER = '/home/kali/Desktop/CapturedData'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+r_server.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
+@r_server.route('/')
 def server_test():
     return 'Welcome to the Triskelion remote server'
 
-@app.route('/upload', methods=['POST'])
+@r_server.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
         return 'No file part', 400
@@ -18,10 +18,10 @@ def upload_file():
         return 'No selected file', 400
     if file:
         filename = file.filename
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        file.save(os.path.join(r_server.config['UPLOAD_FOLDER'], filename))
         return 'File uploaded successfully', 200
 
 if __name__ == '__main__':
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
-    app.run(host='0.0.0.0', port=5000)
+    r_server.run(host='0.0.0.0', port=5000)
